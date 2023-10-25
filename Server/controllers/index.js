e227a282e3419a0cf7c41f8911a3ac0b4b008a98
Gema <gemaramadhan20@gmail.com>
2023-10-26 01:19:42 +0700

    feat: fetch Data mountain

--- Server/controllers/index.js
+++ Server/controllers/index.js
@@ -5,7 +5,20 @@ const baseUrl = "https://ch1news.gemaramadhan.online";
 // const baseUrl = 'http://localhost:3000'
 
 class Controller {
-
+  static async showResult(req, res) {
+    try {
+      const { name } = req.query;
+      console.log(name);
+      const mountain = await Mountain.findOne({
+        where: { name: { [Op.iLike]: `${name}%` } },
+        include: [{ model: Pos }],
+      });
+      res.status(200).json({mountain})
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({message: "ISE", error})
+    }
+  }
 }
 
 module.exports = Controller;
